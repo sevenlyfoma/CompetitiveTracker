bce12ed463cdf3aec1f0328fc36350e160c91697
7lyfoma <7lyfoma@gmail.com>
2026-02-21 13:00:17 +0000

    added base spring boot project

---
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/CompTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.githib.sevenlyfoma.comp_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompTrackerApplication.class, args);
+	}
+
+}
