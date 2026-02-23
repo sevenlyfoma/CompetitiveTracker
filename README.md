4d0491f265bbeb2cea5ee47509f5c951d12f8102
7lyfoma <7lyfoma@gmail.com>
2026-02-23 15:08:55 +0000

    added components facilitating updating of user details with put requests

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ curl --header "Content-Type: application/json" --request POST --data '{"name":"j
 
 curl --header "Content-Type: application/json" --request POST --data '{"name":"jane example","email":"jane@example.com","pronouns":"she/her","rating":1000}' http://localhost:8080/users
 
-curl --header "Content-Type: application/json" --request PUT --data '{"name":"jane example","email":"jane@example.com","pronouns":"she/her they/them","rating":1000}' http://localhost:8080/users/5
+curl --header "Content-Type: application/json" --request PUT --data '{"name":"jane e example","email":"jane@example.com","pronouns":"she/her they/them","rating":1000}' http://localhost:8080/users/11
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,86 @@ function App() {
   )
 }
 
+function UserList() {
+  const [userList, setUserList] = useState([]);
+
+  const [editingId, setEditingId] = useState(null);
+
+  const handleEditClick = (id) => {
+    setEditingId(id);
+  };
+
+  const handleCancel = () => {
+    setEditingId(null);
+  };
+
+  useEffect(() => {
+  console.log("Updated Editing ID State:", editingId);
+  }, [editingId]);
+
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("/users/all");
+      if (!response.ok) {
+        throw new Error(`Server responded with status: ${response.status}`);
+      }
+      const userListJson = await response.json();
+      console.log(userListJson);
+      setUserList(userListJson);
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setUserList([]); 
+    }
+  
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h2>User List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Pronouns</th>
+            <th>Rating</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList
+          .slice() 
+          .sort((a, b) => a.id - b.id)
+          .map((user) => {
+            if (editingId === user.id) {
+              return (
+                <UpdateUserRow key={user.id} user_data={user} onUpdate={fetchUsers} handleCancel={handleCancel}/>
+              )
+            }
+            else{
+              return (
+                <StandardUserRow key={user.id} user={user} handleEditClick={handleEditClick} fetchUsers={fetchUsers}/>
+              )
+            }
+          })}
+          <CreateUserRow key={"Create User Row"} onCreate={fetchUsers}/>
+        </tbody>
+
+      </table>
+    
+    </>
+  )
+ 
+}
+
 /**
  * @param {Object} props
  * @param {number} props.userID - the uniquely identifing id of the user to be deleted
@@ -45,104 +125,43 @@ function UserDeleteButton({userID, onDelete}) {
   
   return (
     <>
-      <button onClick={handleDelete} style={{ color: 'red' }}>
+      <button className='ChangeButton' onClick={handleDelete} style={{ color: 'red' }}>
         Delete User
       </button>
     </>
   )
 }
 
-function UserList() {
-  const [userList, setUserList] = useState([]);
-
-
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch("/users/all");
-      if (!response.ok) {
-        throw new Error(`Server responded with status: ${response.status}`);
-      }
-      const userListJson = await response.json();
-      console.log(userListJson);
-      setUserList(userListJson);
-
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setUserList([]); 
-    }
+function EditUserButton({userID, handleEditClick}){
   
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
   return (
-    <>
-      <h2>User List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Pronouns</th>
-            <th>Rating</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {userList.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.pronouns}</td>
-              <td>{user.rating}</td>
-              <td><UserDeleteButton userID={user.id} onDelete={fetchUsers}/></td>
-            </tr>
-          ))}
-          <CreateUserRow onCreate={fetchUsers}/>
-        </tbody>
-
-      </table>
-    
-    </>
+  
+    <button className='EditButton' onClick={() => {handleEditClick(userID)}} style={{ color: 'blue' }}>
+      Edit
+    </button>
   )
- 
 }
 
-function CreateUserButton({ userData, onCreate }) {
-  const handleCreate = async () => {
-    if (!window.confirm("Are you sure the users data is correct")) return;
-
-    try {
-      const response = await fetch(`/users`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData)
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to create user');
-      }
-
-      onCreate();
-      console.log(`User created uccessfully`);
-    } catch (error) {
-      console.error("Error creating user:", error);
-    }
-  };
-
+function StopEditUserButton({handleCancel}){
+   return (
   
+    <button className='EditButton' onClick={handleCancel} style={{ color: 'blue' }}>
+      Cancel
+    </button>
+  )
+}
+
+function StandardUserRow({user, handleEditClick, fetchUsers}) {
   return (
-    <>
-      <button onClick={handleCreate} style={{ color: 'green' }}>
-        Create User
-      </button>
-    </>
+    <tr>
+      <td><EditUserButton userID={user.id} handleEditClick={handleEditClick}/></td>
+      <td>{user.id}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.pronouns}</td>
+      <td>{user.rating}</td>
+      <td><UserDeleteButton userID={user.id} onDelete={fetchUsers}/></td>
+    </tr>
   )
 }
 
@@ -164,6 +183,7 @@ function CreateUserRow( {onCreate} ) {
   }
 
   return <tr>
+      <td></td>
       <td>
 
       </td>
@@ -185,10 +205,130 @@ function CreateUserRow( {onCreate} ) {
       </td>
 
       <td>
-         <CreateUserButton userData={user} onCreate={onCreate}/>
+         <CreateUserButton userData={user} onCreate={onCreate} setUser={setUser}/>
       </td>
     </tr>
   
+}
+
+function CreateUserButton({ userData, onCreate, setUser }) {
+  const handleCreate = async () => {
+    if (!window.confirm("Are you sure the users data is correct")) return;
+
+    try {
+      const response = await fetch(`/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create user');
+      }
+
+      onCreate();
+      setUser({name: "", email: "", pronouns: "", rating: ""});
+      console.log(`User created uccessfully`);
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
+
+  
+  return (
+    <>
+      <button className='ChangeButton' onClick={handleCreate} style={{ color: 'green' }}>
+        Create User
+      </button>
+    </>
+  )
+}
+
+function UpdateUserRow( {user_data, onUpdate, handleCancel} ) {
+  const [user, setUser] = useState(user_data);
+
+  // useEffect(() => {
+  // console.log("Updated User State:", user);
+  // }, [user]);
+
+  function handleChange(e) {
+    const { name, value, type } = e.target;
+
+    setUser((prevUser) => ({
+      ...prevUser,
+      // [name]: value,
+      [name]: type === 'number' ? Number(value) : value,
+    }));
+  }
+
+  const combinedOnUpdate = () => {
+    onUpdate();
+    handleCancel();
+  }
+
+  return <tr>
+      <td><StopEditUserButton handleCancel={handleCancel}/></td>
+      
+      <td>{user.id}</td>
+
+      <td>
+        <input type="text" name="name" value={user.name} onChange={handleChange}/> 
+      </td>
+
+      <td>
+        <input type="text" name="email" value={user.email} onChange={handleChange}/> 
+      </td>
+
+      <td>
+        <input type="text" name="pronouns" value={user.pronouns} onChange={handleChange}/> 
+      </td>
+
+      <td>
+        <input type="number" name="rating" value={user.rating} onChange={handleChange}/> 
+      </td>
+
+      <td>
+         <UpdateUserButton userData={user} onUpdate={combinedOnUpdate}/>
+      </td>
+    </tr>
+  
+}
+
+function UpdateUserButton({ userData, onUpdate }) {
+  const handleUpdate = async () => {
+    if (!window.confirm("Are you sure the users data is correct")) return;
+
+    console.log(userData.id)
+    try {
+      const response = await fetch(`/users/${userData.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update user');
+      }
+
+      onUpdate();
+      console.log(`User updated successfully`);
+    } catch (error) {
+      console.error("Error updating user:", error);
+    }
+  };
+
+  
+  return (
+    <>
+      <button className='ChangeButton' onClick={handleUpdate} style={{ color: 'blue' }}>
+        Update User
+      </button>
+    </>
+  )
 }
 
 export default App
