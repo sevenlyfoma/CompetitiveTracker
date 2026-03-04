65620f8dc3c638ce61e4536691a5808c1cd9ead5
7lyfoma <7lyfoma@gmail.com>
2026-03-04 18:39:41 +0000

    added boilerplate code for Tournament ORM class

---
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Tournament.java
@@ -0,0 +1,24 @@
+package io.githib.sevenlyfoma.comp_tracker;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tournaments")
+public class Tournament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Use LocalDateTime to match SQL TIMESTAMP
+    @Column(name = "tournament_name", nullable = false)
+    private String tournamentName;
+
+    public Tournament() {}
+    
+}
