367d0e3082126fc6533ce426bcce50fe58815c15
7lyfoma <7lyfoma@gmail.com>
2026-03-15 11:01:45 +0000

    updated ToMa controller and repository to allow getting matches by tournamentID

--- comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Controller/TournamentMatchController.java
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Controller/TournamentMatchController.java
@@ -1,9 +1,11 @@
 package io.githib.sevenlyfoma.comp_tracker.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.githib.sevenlyfoma.comp_tracker.Model.Tournament;
 import io.githib.sevenlyfoma.comp_tracker.Model.TournamentMatch;
 import io.githib.sevenlyfoma.comp_tracker.Model.TournamentMatchRepository;
 
@@ -18,7 +20,16 @@ public class TournamentMatchController {
     }
 
     @GetMapping("/all")
-    public Iterable<TournamentMatch> getAllEntrants() {
+    public Iterable<TournamentMatch> getAllTMatches() {
         return tournamentMatchRepository.findAll();
     }
+
+    @GetMapping("/{tid}")
+     public Iterable<TournamentMatch> getTMatchesByTId(@PathVariable Long tid){
+        var t = Tournament.builder().id(tid).tournamentName(null).closed(null).build();
+        var tournamentEntrantList = tournamentMatchRepository.findByTournament(t);
+        return tournamentEntrantList;
+    }
+
+
 }
--- comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Model/TournamentMatchRepository.java
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Model/TournamentMatchRepository.java
@@ -1,8 +1,10 @@
 package io.githib.sevenlyfoma.comp_tracker.Model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository; 
 
 public interface TournamentMatchRepository extends CrudRepository<TournamentMatch, Long> {
-
+    List<TournamentMatch> findByTournament(Tournament tournament);
 }
 
