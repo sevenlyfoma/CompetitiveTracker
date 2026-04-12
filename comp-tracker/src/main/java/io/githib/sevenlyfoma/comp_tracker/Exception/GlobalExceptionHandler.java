4efea63615181dc35722335b369469be67d48a3b
7lyfoma <7lyfoma@gmail.com>
2026-04-12 19:10:26 +0100

    Added Skeleton For global exception handler

---
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package io.githib.sevenlyfoma.comp_tracker.Exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
