eb876d7b3ef02ecefa2e1984c1a8b3de992d1c94
7lyfoma <7lyfoma@gmail.com>
2026-04-21 18:45:35 +0100

    Added Match Participant Repository

---
+++ comp-tracker/src/main/java/io/githib/sevenlyfoma/comp_tracker/Refactor/MatchParticipantRepository.java
@@ -0,0 +1,8 @@
+package io.githib.sevenlyfoma.comp_tracker.Refactor;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MatchParticipantRepository extends CrudRepository<MatchParticipant, Long>{
+
+}
+
