d399b7a9620a6deb6a46b10bdee99370bfdd4e11
7lyfoma <7lyfoma@gmail.com>
2026-05-03 06:55:00 +0100

    Replace navigation sidebar with material ui version

--- comp-tracker/frontend/src/Layout/Layout.jsx
+++ comp-tracker/frontend/src/Layout/Layout.jsx
@@ -12,9 +12,43 @@ import './Layout.css';
 import { Sidebar, Menu, MenuItem, SubMenu, sidebarClasses} from 'react-pro-sidebar';
 
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+import Grid from '@mui/material/Grid';
+
+import AddIcon from '@mui/icons-material/Add';
+import DataArrayIcon from '@mui/icons-material/DataArray';
+
+function BasicList() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, height:'100vh', bgcolor: 'lightgrey' }}>
+      <nav aria-label="Page Navigation Options">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="createuser">
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText primary="Create New User" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="tournaments">
+              <ListItemIcon>
+                <DataArrayIcon />
+              </ListItemIcon>
+              <ListItemText primary="Tournaments" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
 }
 
 
@@ -25,27 +59,18 @@ function Layout() {
 
 
   return (
-    <div className='layoutDiv'>
-    <Sidebar
-      rootStyles={{
-        [`.${sidebarClasses.container}`]: {
-          backgroundColor: 'transparent',
-          height: '100vh',
-          position: 'sticky',
-          top: 0,
-        },
-      }}
-    >
-      <Menu>
-        <MenuItem> Create User </MenuItem>
-        <MenuItem component={<Link to="/tournaments"/>}>   View Tournaments </MenuItem>
-      </Menu>
-    </Sidebar>
+     <Box sx={{ flexGrow: 1, bgcolor: 'grey' }}>
+        <Grid container spacing={0}>
+          <Grid size={2}>
+            <BasicList />
+          </Grid>
+          <Grid size={10}>
+            <Outlet />
+          </Grid>
+        </Grid>
 
-    <ButtonUsage />
 
-    <Outlet/>
-    </div>
+     </Box>
   );
 }
 
